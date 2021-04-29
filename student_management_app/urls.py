b8f74177151fc00954919fdbcd660506aa61b45a
akshit-adeshra <jwick4298@gmail.com>
2021-04-29 10:19:01 +0530

    Added Home Page for Admin

--- student_management_app/urls.py
+++ student_management_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from . import views, HodViews
 
 urlpatterns = [
-    path('demo/', views.DemoPage),
-    path('', views.LoginPage),
-    path('doLogin/', views.doLogin),
-    path('get_user_details/', views.GetUserDetails),
-    path('logout_user/', views.LogoutUser),
+    path('demo/', views.DemoPage, name="demo"),
+    path('', views.LoginPage, name="login"),
+    path('doLogin/', views.doLogin, name="doLogin"),
+    path('get_user_details/', views.GetUserDetails, name="get_user_details"),
+    path('logout_user/', views.LogoutUser, name="logout_user"),
+    path('admin_home/', HodViews.admin_home, name="admin_home"),
 ]
